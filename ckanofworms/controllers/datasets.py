766e821b40123ba7874121c71a1ad7193feaba82
Emmanuel Raviart <eraviart@easter-eggs.com>
2013-10-04 00:43:26 +0200

    Comment out dataset api1_set controller.

--- ckanofworms/controllers/datasets.py
+++ ckanofworms/controllers/datasets.py
@@ -402,140 +402,140 @@ def api1_related(req):
         )
 
 
-@wsgihelpers.wsgify
-def api1_set(req):
-    ctx = contexts.Ctx(req)
-    headers = wsgihelpers.handle_cross_origin_resource_sharing(ctx)
+#@wsgihelpers.wsgify
+#def api1_set(req):
+#    ctx = contexts.Ctx(req)
+#    headers = wsgihelpers.handle_cross_origin_resource_sharing(ctx)
 
-    assert req.method == 'POST', req.method
+#    assert req.method == 'POST', req.method
 
-    inputs_converters = dict(
-        # Shared secret between client and server
-        api_key = conv.pipe(
-            conv.test_isinstance(basestring),
-            conv.input_to_token,
-            conv.not_none,
-            ),
-        # For asynchronous calls
-        context = conv.test_isinstance(basestring),
-        )
+#    inputs_converters = dict(
+#        # Shared secret between client and server
+#        api_key = conv.pipe(
+#            conv.test_isinstance(basestring),
+#            conv.input_to_token,
+#            conv.not_none,
+#            ),
+#        # For asynchronous calls
+#        context = conv.test_isinstance(basestring),
+#        )
 
-    content_type = req.content_type
-    if content_type is not None:
-        content_type = content_type.split(';', 1)[0].strip()
-    if content_type == 'application/json':
-        inputs, error = conv.pipe(
-            conv.make_input_to_json(),
-            conv.test_isinstance(dict),
-            )(req.body, state = ctx)
-        if error is not None:
-            return wsgihelpers.respond_json(ctx,
-                collections.OrderedDict(sorted(dict(
-                    apiVersion = '1.0',
-                    error = collections.OrderedDict(sorted(dict(
-                        code = 400,  # Bad Request
-                        errors = [error],
-                        message = ctx._(u'Invalid JSON in request POST body'),
-                        ).iteritems())),
-                    method = req.script_name,
-                    params = req.body,
-                    url = req.url.decode('utf-8'),
-                    ).iteritems())),
-                headers = headers,
-                )
-        inputs_converters.update(dict(
-            value = conv.pipe(
-                conv.json_to_dataset_attributes,
-                conv.not_none,
-                ),
-            ))
-    else:
-        # URL-encoded POST.
-        inputs = dict(req.POST)
-        inputs_converters.update(dict(
-            value = conv.pipe(
-                conv.make_input_to_json(),
-                conv.json_to_dataset_attributes,
-                conv.not_none,
-                ),
-            ))
+#    content_type = req.content_type
+#    if content_type is not None:
+#        content_type = content_type.split(';', 1)[0].strip()
+#    if content_type == 'application/json':
+#        inputs, error = conv.pipe(
+#            conv.make_input_to_json(),
+#            conv.test_isinstance(dict),
+#            )(req.body, state = ctx)
+#        if error is not None:
+#            return wsgihelpers.respond_json(ctx,
+#                collections.OrderedDict(sorted(dict(
+#                    apiVersion = '1.0',
+#                    error = collections.OrderedDict(sorted(dict(
+#                        code = 400,  # Bad Request
+#                        errors = [error],
+#                        message = ctx._(u'Invalid JSON in request POST body'),
+#                        ).iteritems())),
+#                    method = req.script_name,
+#                    params = req.body,
+#                    url = req.url.decode('utf-8'),
+#                    ).iteritems())),
+#                headers = headers,
+#                )
+#        inputs_converters.update(dict(
+#            value = conv.pipe(
+#                conv.json_to_dataset_attributes,
+#                conv.not_none,
+#                ),
+#            ))
+#    else:
+#        # URL-encoded POST.
+#        inputs = dict(req.POST)
+#        inputs_converters.update(dict(
+#            value = conv.pipe(
+#                conv.make_input_to_json(),
+#                conv.json_to_dataset_attributes,
+#                conv.not_none,
+#                ),
+#            ))
 
-    data, errors = conv.struct(inputs_converters)(inputs, state = ctx)
-    if errors is not None:
-        return wsgihelpers.respond_json(ctx,
-            collections.OrderedDict(sorted(dict(
-                apiVersion = '1.0',
-                context = inputs.get('context'),
-                error = collections.OrderedDict(sorted(dict(
-                    code = 400,  # Bad Request
-                    errors = [errors],
-                    message = ctx._(u'Bad parameters in request'),
-                    ).iteritems())),
-                method = req.script_name,
-                params = inputs,
-                url = req.url.decode('utf-8'),
-                ).iteritems())),
-            headers = headers,
-            )
+#    data, errors = conv.struct(inputs_converters)(inputs, state = ctx)
+#    if errors is not None:
+#        return wsgihelpers.respond_json(ctx,
+#            collections.OrderedDict(sorted(dict(
+#                apiVersion = '1.0',
+#                context = inputs.get('context'),
+#                error = collections.OrderedDict(sorted(dict(
+#                    code = 400,  # Bad Request
+#                    errors = [errors],
+#                    message = ctx._(u'Bad parameters in request'),
+#                    ).iteritems())),
+#                method = req.script_name,
+#                params = inputs,
+#                url = req.url.decode('utf-8'),
+#                ).iteritems())),
+#            headers = headers,
+#            )
 
-    api_key = data['api_key']
-    account = model.Account.find_one(
-        dict(
-            api_key = api_key,
-            ),
-        as_class = collections.OrderedDict,
-        )
-    if account is None:
-        return wsgihelpers.respond_json(ctx,
-            collections.OrderedDict(sorted(dict(
-                apiVersion = '1.0',
-                context = data['context'],
-                error = collections.OrderedDict(sorted(dict(
-                    code = 401,  # Unauthorized
-                    message = ctx._('Unknown API Key: {}').format(api_key),
-                    ).iteritems())),
-                method = req.script_name,
-                params = inputs,
-                url = req.url.decode('utf-8'),
-                ).iteritems())),
-            headers = headers,
-            )
-    # TODO: Handle account rights.
+#    api_key = data['api_key']
+#    account = model.Account.find_one(
+#        dict(
+#            api_key = api_key,
+#            ),
+#        as_class = collections.OrderedDict,
+#        )
+#    if account is None:
+#        return wsgihelpers.respond_json(ctx,
+#            collections.OrderedDict(sorted(dict(
+#                apiVersion = '1.0',
+#                context = data['context'],
+#                error = collections.OrderedDict(sorted(dict(
+#                    code = 401,  # Unauthorized
+#                    message = ctx._('Unknown API Key: {}').format(api_key),
+#                    ).iteritems())),
+#                method = req.script_name,
+#                params = inputs,
+#                url = req.url.decode('utf-8'),
+#                ).iteritems())),
+#            headers = headers,
+#            )
+#    # TODO: Handle account rights.
 
-    dataset_attributes = data['value']
-    dataset = model.Dataset.find_one(dataset_attributes['_id'], as_class = collections.OrderedDict)
-    if dataset is not None and dataset.draft_id != dataset_attributes.get('draft_id'):
-        # The modified dataset is not based on the latest version of the dataset.
-        return wsgihelpers.respond_json(ctx,
-            collections.OrderedDict(sorted(dict(
-                apiVersion = '1.0',
-                context = data['context'],
-                error = collections.OrderedDict(sorted(dict(
-                    code = 409,  # Conflict
-                    message = ctx._('Wrong version: {}, expected: {}').format(dataset_attributes.get('draft_id'),
-                        dataset.draft_id),
-                    ).iteritems())),
-                method = req.script_name,
-                params = inputs,
-                url = req.url.decode('utf-8'),
-                value = conv.check(conv.method('turn_to_json'))(dataset, state = ctx),
-                ).iteritems())),
-            headers = headers,
-            )
-    dataset = model.Dataset(**dataset_attributes)
-    dataset.save(ctx, safe = True)
+#    dataset_attributes = data['value']
+#    dataset = model.Dataset.find_one(dataset_attributes['_id'], as_class = collections.OrderedDict)
+#    if dataset is not None and dataset.draft_id != dataset_attributes.get('draft_id'):
+#        # The modified dataset is not based on the latest version of the dataset.
+#        return wsgihelpers.respond_json(ctx,
+#            collections.OrderedDict(sorted(dict(
+#                apiVersion = '1.0',
+#                context = data['context'],
+#                error = collections.OrderedDict(sorted(dict(
+#                    code = 409,  # Conflict
+#                    message = ctx._('Wrong version: {}, expected: {}').format(dataset_attributes.get('draft_id'),
+#                        dataset.draft_id),
+#                    ).iteritems())),
+#                method = req.script_name,
+#                params = inputs,
+#                url = req.url.decode('utf-8'),
+#                value = conv.check(conv.method('turn_to_json'))(dataset, state = ctx),
+#                ).iteritems())),
+#            headers = headers,
+#            )
+#    dataset = model.Dataset(**dataset_attributes)
+#    dataset.save(ctx, safe = True)
 
-    return wsgihelpers.respond_json(ctx,
-        collections.OrderedDict(sorted(dict(
-            apiVersion = '1.0',
-            context = data['context'],
-            method = req.script_name,
-            params = inputs,
-            url = req.url.decode('utf-8'),
-            value = conv.check(conv.method('turn_to_json'))(dataset, state = ctx),
-            ).iteritems())),
-        headers = headers,
-        )
+#    return wsgihelpers.respond_json(ctx,
+#        collections.OrderedDict(sorted(dict(
+#            apiVersion = '1.0',
+#            context = data['context'],
+#            method = req.script_name,
+#            params = inputs,
+#            url = req.url.decode('utf-8'),
+#            value = conv.check(conv.method('turn_to_json'))(dataset, state = ctx),
+#            ).iteritems())),
+#        headers = headers,
+#        )
 
 
 @wsgihelpers.wsgify
@@ -918,7 +918,7 @@ def route_api1(environ, start_response):
 def route_api1_class(environ, start_response):
     router = urls.make_router(
         ('GET', '^/?$', api1_index),
-        ('POST', '^/?$', api1_set),
+#        ('POST', '^/?$', api1_set),
         ('POST', '^/ckan/?$', api1_set_ckan),
         ('GET', '^/related/?$', api1_related),
         (None, '^/(?P<id>[^/]+)(?=/|$)', route_api1),
